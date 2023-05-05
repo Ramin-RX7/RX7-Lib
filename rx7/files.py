ba3bb393c58bf7d5bc3b9a3cfd3f405d3ecfdc6b
Ramin RX7 <rawmin.rx@gmail.com>
2023-05-06 00:52:30 +0430

    update docstring for all files module functions

--- rx7/files.py
+++ rx7/files.py
@@ -32,10 +32,7 @@ from typing import Text,Literal,Generator
 
 
 def size(path:str) -> int:
-    '''
-    return size of the file in byte(s).
-    Also work on directories.
-    '''
+    '''return size of the file/directory in bytes.'''
     if isdir:
         total_size = 0
         for dirpath, dirnames, filenames in _os.walk(path):
@@ -48,10 +45,11 @@ def size(path:str) -> int:
     return _os.path.getsize(path)
     #rooye pooshe emtehan she
 
+
 def remove(path:str,force:bool=False) -> None:
     '''
-    Use this to delete a file or a directory.
-    If force is True it will delete non-empty directories.
+    This will delete both a file or directory.
+    If `force` is True it will delete non-empty directories.
     '''
     if _os.path.isfile(path):
         _os.remove(path)
@@ -66,66 +64,57 @@ def remove(path:str,force:bool=False) -> None:
                                '(Use force=True as an argument of remove function to remove non-empty directories.)') from None
 delete = remove
 
+
 def rename(old_name:str, new_name:str) -> None:
-    '''Rename files with this function.'''
+    '''Rename files.'''
     _os.rename(old_name,new_name)
 
+
 def abspath(path:str) -> str:
-    '''
-    return absolute path of given path.
-    '''
+    '''return absolute path of given path.'''
     return _os.path.abspath(path)
 
+
 def exists(path:str) -> bool:
-    '''
-    Search for the file And Returns a boolean.
-    if file exists: True
-    else: False
-    '''
+    '''Test whether a path exists'''
     return _os.path.exists(path)
 
+
 def mdftime(path:str) -> float:
-    '''
-    Get last modify time of the path.
-    '''
+    '''Get last modify time of the path.'''
     return _os.path.getmtime(path)
 
+
 def acstime(path:str) -> float:
-    '''
-    Get last access time of the path.
-    '''
+    '''Get last access time of the path.'''
     return _os.path.getatime(path)
     # change to date bayad biad
 
+
 def move(src:str, dest:str) -> None:
-    '''
-    Move (cut) file/directory from src to dst.
-    '''
+    '''Moves (cuts) file/directory from src to dst.'''
     _shutil.move(src,dest)
-    #live_path= dst
-    #works for dirs too or not?
+
 
 def copy(src:str, dest:str, preserve_metadata:bool=True) -> None:
     '''
     Copy the file from src to destination.
-    preserve_metadata is for preserving metadata of file when copying.
-    (You can use it instead of rename too.
-     e.g:
-        copy('D:\\Test.py','E:\\Ali.py')
-        (It copies Test.py to E drive and renames it to Ali.py)
-     )
+    preserve_metadata is for preserving metadata of files when copying.
     '''
     if isdir(src):
         _shutil.copytree(src,dest)
     else:
-        if preserve_metadata: _shutil.copy2(src,dest)
-        else: _shutil.copy(src,dest)
+        if preserve_metadata:
+            _shutil.copy2(src,dest)
+        else:
+            _shutil.copy(src,dest)
+
 
 def hide(path:str, mode:bool=True) -> None:
     '''
-    Hide file or folder.
-    If mode==False: unhides the file/folder
     (WINDOWS ONLY)
+    Hide file/folder.
+    If mode==False: unhides the file/folder
     '''
     try:
         import win32api, win32con
@@ -136,36 +125,33 @@ def hide(path:str, mode:bool=True) -> None:
     else:
         win32api.SetFileAttributes(path,win32con.FILE_ATTRIBUTE_NORMAL)
 
+
 def read_only(path:str, mode:bool=True) -> None:
     '''
-    Make file attribute read_only.
+    Make the given path read_only.
     If mode==False: makes 'not read_only'
     '''
-    if type(mode)==bool:
-        from stat import S_IREAD,S_IWUSR
-        if mode==True:
-            _os.chmod(path, S_IREAD)
-        elif mode==False:
-            _os.chmod(path, S_IWUSR)
-    else:
-        raise Exception('Second argumant (mode) should be boolean.')
+    from stat import S_IREAD,S_IWUSR
+    if mode==True:
+        _os.chmod(path, S_IREAD)
+    elif mode==False:
+        _os.chmod(path, S_IWUSR)
+
 
 def read(file_path:str) -> str:
     '''
-    This can help you to read your file faster.
-    Example:
-        read('C:\\users\\Jack\\test.txt')
-        ==> "Content of 'test.txt' will be shown."
+    Get content of a file.
     '''
     with open(file_path) as f:
-        FileR= f.read()
-    return FileR
+        content = f.read()
+    return content
+
 
 def write(file_path:str, text:Text=None, mode='replace', start='') -> None:
     '''
-    With this method you can change content of the file.
+    Modify content of the given file.
     file:   File you want to change its content.
-    content:   Content you want to add to file.
+    text:   Content you want to add to file.
     mode:   Type of writing method.
          'a' or 'continue' for add content to end of the file.
          'w' or 'replace'  for overwriting to file content.
@@ -173,52 +159,54 @@ def write(file_path:str, text:Text=None, mode='replace', start='') -> None:
     '''
     if mode in ("w",'replace'):
         op= open(file_path,mode='w')
-        if text==None:
-            text= input('Type what you want.\n\n')
         op.write(text)
         op.close()
     elif mode in ("a",'continue'):
         op=open(file_path,'a')
-        if text==None:
-            text= input('Type what you want to add in the end of the file.\n\n')
         op.write(text)
         op.close()
     else:
         raise ValueError('mode can only be: replace(default) or continue Not "{0}"'.format(mode))
 
+
 def isdir(path:str) -> bool:
+    """Return true if the pathname refers to an existing directory"""
     return _os.path.isdir(path)
 
+
 def isfile(path:str):
+    """Test whether a path is a regular file"""
     return _os.path.isfile(path)
 
+
 def is_readonly(path:str):
-    '''
-    Return True if path is readonly else False.
-    (May Not Work in Linux)
-    '''
+    '''Return True if path is readonly else False. (May Not Work in Linux)'''
     return  not _os.access(path, _os.R_OK)
+
 
 def is_hidden(path:str):
     """
     Check whether a file is presumed hidden, either because
     the pathname starts with dot or because the platform
     indicates such.
-    Return True if File or Directory is hidden.
-    (Work on both Linux and Windows)
+    Return True if File or Directory is hidden. (Work on both Linux and Windows)
     """
     import platform
     full_path = _os.path.abspath(path)
     name = _os.path.basename(full_path)
-    def no(path): return False
+    def no(path):
+        return False
     platform_hidden = globals().get('is_hidden_' + platform.system(), no)
     return name.startswith('.') or platform_hidden(full_path)
 
+
 def is_hidden_Windows(path:str):
+    """Detemines wether a file is hidden or not."""
     import ctypes
     res = ctypes.windll.kernel32.GetFileAttributesW(path)
     assert res != -1
     return bool(res & 2)
+
 
 def search_file(pattern:str, path:str='./', return_mode:Literal[list,Generator]=list):
     '''
@@ -233,26 +221,24 @@ def search_file(pattern:str, path:str='./', return_mode:Literal[list,Generator]=
     -  'mydir/**/*.py'   :    search for all python files in path/mydir/ and all of its sub-directories.
     '''
     if return_mode not in (list,Generator):
-        raise ValueError(f"return_mode should be either 'list' or 'generator'  not {return_mode}")
+        raise ValueError(f"return_mode should be either 'list' or 'Generator'  not {return_mode}")
     import glob
-    #print(_os.path.join(path,pattern))
     if return_mode=='list': return glob.glob(_os.path.join(path,pattern), recursive=True)
     else: return glob.iglob(_os.path.join(path,pattern), recursive=True)
 
+
 def search_content(path:str,word:str):
+    """returns list of files in path that includes the given text."""
     ALL= [val for sublist in [[_os.path.join(i[0], j) for j in i[2]] for i in _os.walk(path)] for val in sublist]
-    '''lst=[]
-    for file in ALL:
-        if word in rx.read(file):
-            lst.append(file)
-    return lst'''
     return [file for file in ALL if word in open(file).read()]
 
+
 def mkdir(path:str):
+    """Make directory. Chained path is also accepted"""
     path = _os.path.normpath(path)
     NEW= ''
-    for FILE in path.split('\\'):
-        NEW+= FILE+'\\'
+    for FILE in path.split('/'):
+        NEW+= FILE+'/'
         try:
             _os.mkdir(NEW)
         except (FileExistsError,FileNotFoundError):
@@ -293,22 +279,30 @@ def generate_tree(dir_path:str, level: int=-1, limit_to_directories: bool=False,
         RETURN+=f'\n{directories} directories' + (f', {files} files' if files else '')
     return RETURN
 
+
 def get_drives() -> tuple:
-    """WINDOWS ONLY
+    """(WINDOWS ONLY)
     Gets devices and drives in windows
     """
     return (drive for drive in "CDEFGHIJKLMNOPQRSTUVWXYZ" if exists(f"{drive}:/"))
+
 
 def basename(path:str) -> str:
     """Returns the final component of a pathname"""
     return _os.path.basename(path)
 
+
 def join_paths(path:str, *paths) -> str:
+    """Joins all the given paths together"""
     return _os.path.join(path,*paths)
+
 
 def dirname(path:str) -> str:
     """Returns the directory component of a pathname"""
     return _os.path.dirname(path)
+
+
+
 class MEMBERS:
 
     def all_exactdir(dir:str):
